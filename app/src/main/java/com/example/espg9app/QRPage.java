afbf3c21e65d9d2635e8260ee2f9b10d7896d97b
Dario Cline <darioclinemp3@gmail.com>
2023-02-14 10:22:54 +0000

    made small changes to make sure that the QR code generator is working

--- app/src/main/java/com/example/espg9app/QRPage.java
+++ app/src/main/java/com/example/espg9app/QRPage.java
@@ -1,10 +1,15 @@
 package com.example.espg9app;
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
@@ -26,16 +31,19 @@ public class QRPage extends Activity {
         edit_input = findViewById(R.id.edit_input);
         bt_generate = findViewById(R.id.bt_generate);
         iv_qr = findViewById(R.id.iv_qr);
+        TextView url_link = (TextView) findViewById(R.id.url_link);
 
         bt_generate.setOnClickListener(v-> {
             generateQR();
         });
+
+
     }
     private void generateQR(){
         String text = edit_input.getText().toString().trim();
         MultiFormatWriter writer = new MultiFormatWriter();
         try {
-            BitMatrix matrix = writer.encode(text, BarcodeFormat.QR_CODE,400,400);
+            BitMatrix matrix = writer.encode(text, BarcodeFormat.QR_CODE,1000,1000);
             BarcodeEncoder encoder = new BarcodeEncoder();
             Bitmap bitmap = encoder.createBitmap(matrix);
             iv_qr.setImageBitmap(bitmap);
