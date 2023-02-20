a23ec6b4f2f52bc00f2c01cea8da1bcd91a291f4
Dario Cline <darioclinemp3@gmail.com>
2023-02-20 14:52:24 +0000

    got the reader to get to the camera

--- app/src/main/java/com/example/espg9app/QRReader.java
+++ app/src/main/java/com/example/espg9app/QRReader.java
@@ -3,15 +3,19 @@ package com.example.espg9app;
 import android.app.AlertDialog;
 import android.content.ClipData;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
-
 
 public class QRReader extends AppCompatActivity {
 
@@ -30,25 +34,37 @@ public class QRReader extends AppCompatActivity {
     }
 
     private void scanCode(){
-        ScanOptions options = new ScanOptions();
-        options.setPrompt("Use the volume button to turn the flash on");
+        IntentIntegrator options = new IntentIntegrator(QRReader.this);
+        options.setPrompt("Turn volume up to turn the flash on");
         options.setBeepEnabled(true);
         options.setOrientationLocked(true);
         options.setCaptureActivity(CaptureAct.class);
-        barLauncher.launch(options);
+        options.initiateScan();
     }
+// check this part of the code again
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(),result ->{
-        if(result.getContents() != null){
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+
+        if (intentResult.getContents() != null) {
             AlertDialog.Builder builder = new AlertDialog.Builder(QRReader.this);
+
             builder.setTitle("Result");
-            builder.setMessage(result.getContents());
+            builder.setMessage(intentResult.getContents());
             builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
                     dialogInterface.dismiss();
                 }
-            }).show();
+            });
+            builder.show();
+        } else {
+            Toast.makeText(this, "rhatted", Toast.LENGTH_SHORT).show();
         }
-    });
+
+    }
+
+
 }
