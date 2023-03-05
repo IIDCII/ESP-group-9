7ad42ec5a2de832fa4b763847f7e6c0f9b8a5619
Dario Cline <darioclinemp3@gmail.com>
2023-03-05 22:12:01 +0000

    Added the edit profile UI. I still need to edit the Java file

---
+++ app/src/main/java/com/example/espg9app/EditProfileBusiness.java
@@ -0,0 +1,13 @@
+package com.example.espg9app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditProfileBusiness extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceBundle) {
+        super.onCreate(savedInstanceBundle);
+        setContentView(R.layout.editprofilebusiness);
+    }
+}
