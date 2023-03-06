a326b2ca83171808a78bf2bc4f5bdd3e3608447c
Dario Cline <darioclinemp3@gmail.com>
2023-03-06 15:00:41 +0000

    just initialised the project

---
+++ app/src/main/java/com/example/espg9app/BusinessVoucherList.java
@@ -0,0 +1,16 @@
+package com.example.espg9app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BusinessVoucherList extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.businessvoucherlist);
+   }
+}
